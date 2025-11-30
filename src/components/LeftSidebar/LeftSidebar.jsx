976593f949abd1c7ac606fefce8058d61ef47bf3
suyog-2005 <suyogkvrwf2020.2022@gmail.com>
2025-12-01 01:00:51 +0530

    LeftSideBar error fixed

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -3,26 +3,26 @@ import assets from '../../assets/assets'
 import './LeftSidebar.css'
 const LeftSidebar = () => {
   return (
-    <div className ="ls">
+    <div className="ls">
       <div className="ls-top">
         <div className="ls-nav">
-            <img src={assets.logo} className ='logo' alt=""/>
-            <div className="menu">
-                <img src={assets.menu_icon} alt="" />
+          <img src={assets.logo} className='logo' alt="" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder='search-here' />
+        </div>
+        <div className="ls-list">
+          <div className="friends">
+            <img src={assets.profile_img} alt="" />
+            <div>
+              <p>Richard Sanford</p>
+              <span>Hello, How are You</span>
             </div>
-            <div className="ls-search">
-                <img src={assets.search_icon} alt=""/>
-                <input type="text" placeholder='search-here' />
-            </div>
-            <div className="ls-list">
-                <div className="friends">
-                    <img src={assets.profile_img} alt="" />
-                    <div>
-                        <p>Richard Sanford</p>
-                        <span>Hello, How are You</span>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
       </div>
     </div>
