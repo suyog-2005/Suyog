c6ae4af08d947ea674b335ed6b6bc93f49cafa4b
suyog-2005 <suyogkvrwf2020.2022@gmail.com>
2025-12-02 14:39:24 +0530

    Chat Page Complete

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 const ProfileUpdate = () => {
   return (
-    <div>
+    <div className='profile'>
+      <div className="profile-container">
+        <form >
+          <h3>Prodile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id='avatar' accept='.png , .jpg, .jpeg ' hidden/>
+            <img src={assets.avatar_icon} alt="" />
+          </label>
+          <input type="text" placeholder='Your Name' required/>
+          <textarea placeholder='Write Profile bio' required></textarea>
+          <button type='submit'>Save</button>
+        </form>
+        <img className='profile-pic' src={assets.logo_icon} alt="" />
+      </div>
       we are on the profile update page
     </div>
   )
