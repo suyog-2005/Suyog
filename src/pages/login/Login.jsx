9c3abbea6716f69e0163ec75e205caf818135fe5
suyog-2005 <suyogkvrwf2020.2022@gmail.com>
2025-12-03 22:22:35 +0530

    Front end complete

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react'
 import './login.css'
 import assets from '../../assets/assets'
+import { signup } from '../../config/firebase.js'
 const Login = () => {
 
   const [currState, setCurrState] = useState("Login");
+
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = (e) =>{
+    e.preventDefault();
+    if(currState === "Sign Up"){
+      signup(userName, email, password);
+    }
+    console.log("Form submitted");
+  }
   return (
     <div className = 'login'>
       <img src={assets.logo_big} alt="" className="logo"/>
-      <form className = "login-form">
+      <form onSubmit={onSubmitHandler} className = "login-form">
         <h2>{currState}</h2>
-        {currState === "Sign Up"?<input type="text" placeholder='Username'className="form-input" required/> : null}
-        <input type="email" placeholder='Email address' className="form-input" required/>
-        <input type="password" placeholder= 'Password'className="form-input" required/>
-        <button type = 'submit'>{currState === "Sign Up"?"Create account" : "Sign In"}</button>
+        {currState === "Sign Up"?<input onChange = {(e) =>setUserName(e.target.value)} value = {userName} type="text" placeholder='Username'className="form-input" required/> : null}
+        <input onChange = {(e)=>setEmail(e.target.value)} value = {email} type="email" placeholder='Email address' className="form-input" required/>
+        <input onChange = {(e)=>setPassword(e.target.value)} value = {password} type="password" placeholder= 'Password'className="form-input" required/>
+        <button type = 'submit' >{currState === "Sign Up"?"Create account" : "Sign In"}</button>
         <div className="login-term">
           <input type="checkbox" />
           <p>Agree to terms of use & privacy policy.</p>
